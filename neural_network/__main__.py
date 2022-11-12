1e8effcb1a48f3fcc4fdbf7d1d23f073fecbdc4c
bendico765 <gianlucamorcaldi@gmail.com>
2022-11-12 13:57:37 +0100

    Added loss_function_parameter to __main__

--- neural_network/__main__.py
+++ neural_network/__main__.py
@@ -5,55 +5,57 @@ import neural_network.utils
 
 
 if __name__ == "__main__":
-    dataset_attribute_columns = ["a1", "a2", "a3", "a4", "a5", "a6"]
-    dataset_class_column = "class"
-    number_inputs = len(dataset_attribute_columns)
-    layer_sizes = (2, 2, 1)
+	dataset_attribute_columns = ["a1", "a2", "a3", "a4", "a5", "a6"]
+	dataset_class_column = "class"
+	number_inputs = len(dataset_attribute_columns)
+	layer_sizes = (2, 2, 1)
+	derivative_loss_function = lambda expected_outputs,  real_outputs: expected_outputs - real_outputs
 
-    tr_df, vl_df, _ = neural_network.utils.split_samples(
-        df=datasets.read_monk1()[0],
-        tr_size=0.7,
-        vl_size=0.3,
-        ts_size=0.0
-    )
-    n_trials = 5
+	tr_df, vl_df, _ = neural_network.utils.split_samples(
+		df=datasets.read_monk1()[0],
+		tr_size=0.7,
+		vl_size=0.3,
+		ts_size=0.0
+	)
+	n_trials = 5
 
-    tr_inputs = tr_df[dataset_attribute_columns].to_numpy()
-    tr_outputs = tr_df[dataset_class_column].to_numpy()
-    vl_inputs = vl_df[dataset_attribute_columns].to_numpy()
-    vl_outputs = vl_df[dataset_class_column].to_numpy()
+	tr_inputs = tr_df[dataset_attribute_columns].to_numpy()
+	tr_outputs = tr_df[dataset_class_column].to_numpy()
+	vl_inputs = vl_df[dataset_attribute_columns].to_numpy()
+	vl_outputs = vl_df[dataset_class_column].to_numpy()
 
-    trials = []
-    for _ in range(n_trials):
-        classifier = MLClassifier(
-            number_inputs=number_inputs,
-            layer_sizes=layer_sizes,
-            activation_functions=(
-                ActivationFunctions.Linear(),
-                ActivationFunctions.Linear(),
-                ActivationFunctions.Sigmoid()
-            ),
-            #regularization_term=0.01,
-            batch_size=10,
-            learning_rate=0.001,
-            n_epochs=200,
-            verbose=False
-        )
-        # training model
-        classifier.fit(tr_inputs, tr_outputs)
-        print("Done training")
+	trials = []
+	for _ in range(n_trials):
+		classifier = MLClassifier(
+			number_inputs=number_inputs,
+			layer_sizes=layer_sizes,
+			activation_functions=(
+				ActivationFunctions.Linear(),
+				ActivationFunctions.Linear(),
+				ActivationFunctions.Sigmoid()
+			),
+			derivative_loss_function=derivative_loss_function,
+			#regularization_term=0.01,
+			batch_size=10,
+			learning_rate=0.001,
+			n_epochs=200,
+			verbose=False
+		)
+		# training model
+		classifier.fit(tr_inputs, tr_outputs)
+		print("Done training")
 
-        # validating result
-        correct_predictions = 0
-        for (input, expected_output) in zip(vl_inputs, vl_outputs):
-            real_output = classifier.predict(input)[0]
-            if round(real_output) == expected_output:
-                correct_predictions += 1
+		# validating result
+		correct_predictions = 0
+		for (input, expected_output) in zip(vl_inputs, vl_outputs):
+			real_output = classifier.predict(input)[0]
+			if round(real_output) == expected_output:
+				correct_predictions += 1
 
-        trials.append(100 * (correct_predictions / len(vl_df)))
+		trials.append(100 * (correct_predictions / len(vl_df)))
 
-    print(f"min: {min(trials)}")
-    print(f"max: {max(trials)}")
-    avg = lambda l: sum(l)/len(l) if len(l) != 0 else 0
-    print(f"avg: {avg(trials)}")
+	print(f"min: {min(trials)}")
+	print(f"max: {max(trials)}")
+	avg = lambda l: sum(l)/len(l) if len(l) != 0 else 0
+	print(f"avg: {avg(trials)}")
 
