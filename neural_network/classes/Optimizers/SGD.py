528e35c28f01d4715826ae1de7caffe89a211cba
filippo <filibiondi2000@gmail.com>
2022-11-15 14:47:39 +0100

    Implemented Nesterov Momentum

--- neural_network/classes/Optimizers/SGD.py
+++ neural_network/classes/Optimizers/SGD.py
@@ -4,14 +4,22 @@ class SGD:
         self.momentum = momentum
         self.regularization = regularization
         self.nesterov = nesterov
-        self.old_gradients = []
+        self.old_deltas = []
 
-    def apply(self, layers, gradients):
-        if not self.old_gradients:
-            for layer, gradient in zip(layers, gradients):
-                layer.weights = layer.weights + self.learning_rate * gradient - 2 * self.regularization * layer.weights
-            self.old_gradients = list(gradients)
+    def apply(self, layers, deltas):
+        if not self.old_deltas:
+            for layer, delta in zip(layers, deltas):
+                layer.weights = layer.weights + self.learning_rate * delta - 2 * self.regularization * layer.weights
+            self.old_deltas = list(deltas)
         else:
-            for layer, gradient, old_gradient in zip(layers, gradients, self.old_gradients):
-                layer.weights = layer.weights + self.learning_rate * gradient - 2 * self.regularization * layer.weights + self.momentum * old_gradient
-            self.old_gradients = list(gradients)
+            if self.nesterov:
+                for layer, delta, old_delta in zip(layers, deltas, self.old_deltas):
+                    layer.weights = layer.weights + self.learning_rate * delta - 2 * self.regularization * layer.weights + self.momentum * old_delta
+                self.old_deltas = list(deltas)
+            else:
+                new_deltas = []
+                for layer, delta, old_delta in zip(layers, deltas, self.old_deltas):
+                    new_delta = self.learning_rate * delta + self.momentum * old_delta
+                    new_deltas.append(new_delta)
+                    layer.weights = layer.weights + new_delta - 2 * self.regularization * layer.weights
+                self.old_deltas = new_deltas
