9bc2dd6c7c4bf8642fb2d0bf60da521a0fe429bc
bendico765 <gianlucamorcaldi@gmail.com>
2022-12-23 17:52:32 +0100

    (maybe) implemented parallelism

--- neural_network/classes/Validation/TunerCV.py
+++ neural_network/classes/Validation/TunerCV.py
@@ -1,7 +1,7 @@
 from neural_network.classes.Validation import ConfigurationGenerator
 from neural_network.classes.Validation.K_fold import K_fold
 from neural_network.classes.Results import ValidationResult, ResultCollection
-
+from concurrent.futures import ProcessPoolExecutor
 
 class TunerCV:
     """
@@ -28,6 +28,31 @@ class TunerCV:
         self.results = None
         self.verbose = verbose
 
+    @staticmethod
+    def fit_configuration(params):
+        config, model_builder, trainval_inputs, trainval_outputs, n_fold, verbose = params
+        if verbose:
+            print("Building model with the following configuration:", config)
+
+        fold_results = []
+        k_fold = K_fold(trainval_inputs.shape[0], n_fold)
+        for (fold_tr_indexes, fold_vl_indexes) in k_fold.get_folds():
+            model = model_builder(config)
+
+            fold_tr_inputs, fold_tr_outputs = trainval_inputs[fold_tr_indexes], trainval_outputs[fold_tr_indexes]
+            fold_vl_inputs, fold_vl_outputs = trainval_inputs[fold_vl_indexes], trainval_outputs[fold_vl_indexes]
+
+            result = model.fit(
+                fold_tr_inputs,
+                fold_tr_outputs,
+                [fold_vl_inputs, fold_vl_outputs]
+            )
+
+            fold_results.append(result)
+
+        return ValidationResult(config, fold_results)
+        #self.results.add_result(fold_results)
+
     def fit(self, trainval_inputs, trainval_outputs):  # TODO  parallelize
         """
         Do the model selection using K-fold cross validation to measure metrics on validation set
@@ -35,14 +60,14 @@ class TunerCV:
         Return:
             ResultCollection: the collection of all results obtained during the model selection
         """
-        k_fold = K_fold(trainval_inputs.shape[0], self.n_fold)
         self.results = ResultCollection()
-        for config in self.configurations:
-
+        """
+        for config in configurations:
             if self.verbose:
                 print("Building model with the following configuration:", config)
 
             fold_results = []
+            k_fold = K_fold(trainval_inputs.shape[0], self.n_fold)
             for (fold_tr_indexes, fold_vl_indexes) in k_fold.get_folds():
                 model = self.model_builder(config)
 
@@ -58,8 +83,24 @@ class TunerCV:
                 fold_results.append(result)
 
             fold_results = ValidationResult(config, fold_results)
+        """
+        configurations_params = [
+            (
+                config,
+                self.model_builder,
+                trainval_inputs,
+                trainval_outputs,
+                self.n_fold,
+                self.verbose
+            )
+            for config in self.configurations
+        ]
+        with ProcessPoolExecutor() as executor:
+            configurations_validation_result = executor.map(TunerCV.fit_configuration, configurations_params)
 
-            self.results.add_result(fold_results)
+        # collecting the results for each runned configuration
+        for configuration_results in configurations_validation_result:
+            self.results.add_result(configuration_results)
         return self.results
 
 
